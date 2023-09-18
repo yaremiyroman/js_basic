0a1db240f9293f811c7e5384151813538ec65aec
roman-yaremiy <ryaremiy@conceptsoft.io>
2023-09-18 22:45:54 +0300

    #1 scripts added

---
+++ #1/converter.js
@@ -0,0 +1,7 @@
+celsius = prompt('Enter Celsius: ', 0);
+
+// (0°C × 9/5) + 32 = 32°F
+
+fahrenheit = (celsius * (9 / 5)) + 32;
+
+alert(fahrenheit);
---
+++ #1/script.js
@@ -0,0 +1,32 @@
+// alert('Зовнішній файл');
+
+// console.log('External file');
+// console.log(  'Hello, world!'    );
+
+// document.write('Some String');
+
+/* console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!'); */
+// console.log(1234);
+// console.log('1234');
+
+
+// x = 333;
+
+// console.log(x);
+
+// stroka = 'any string';
+
+// console.log(stroka);
+
+// alert('Alert');
+
+// num = prompt('Enter number: ', 0);
+
+// console.log(num);
+
+// document.write(num);
+
+// alert(num);
