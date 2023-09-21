cd4b7af647d95fcb67227e0f4c0e5d2cbddce6b7
roman-yaremiy <ryaremiy@conceptsoft.io>
2023-09-21 21:23:57 +0300

    lesson 2 completed

---
+++ 2/script.js
@@ -0,0 +1,289 @@
+// Конкатенація строк
+
+// first = 'random string';
+// second = 'second random string';
+
+// console.log(first);
+// console.log(second);
+
+// third = first + ' ' + ' any sting ' + second;
+// third = first + ' ' + ' any sting  sting\r\n sting  sting  sting  sting here ' + second;
+
+// console.log(third);
+
+
+
+
+// Строковий літерал
+
+// bigString = `Any new string will be here`;
+
+// bigStringWithBreak = `Any new string
+// will be here`;
+
+
+// count = 15;
+// secondCount = 25;
+
+// bigStringInterpolate = `${count + 9 - 10 + 25} apples`;
+
+// bigStringInterpolate2 = `${count + secondCount} apples`;
+
+// console.log(bigStringInterpolate2);
+
+
+
+// HW
+// userName = prompt('Say your name', '');
+// console.log(userName);
+
+// alert('My name is ' + userName + '!!! Hello!');
+
+// alert(`My name is ${userName}!!! Hello!`);
+
+
+
+
+// Змінні ES4
+
+
+// var var3 = 'text';
+// console.log(var3);
+
+// var3 = 'another value';
+// console.log(var3);
+
+// var SOME_CONST = 'const';
+// console.log(SOME_CONST);
+
+
+
+
+
+
+
+// Змінні ES6
+
+// const variable = 'some text';
+// console.log(variable);
+
+
+// let var_2 = 142;
+// console.log(var_2);
+
+// var_2 = 42412;
+
+// Reserved words
+// name = prompt('PROMNT');
+// var var = asdfasf;
+
+// good
+// let myNewFreshBrowser_1;
+// let myNewFreshBrowser_2;
+// let myNewFreshBrowser_3;
+
+// bad
+// let 124dsgsdg;
+// let @#$%^&;
+// console.log(lowerCAsE);
+
+// let lowerCase = 1;
+// let lowerCASE = 2;
+
+// let sometimes;
+// console.log(sometimes);
+
+// let another = 'some value';
+// console.log(another);
+
+// let someString = '';
+// console.log(someString);
+
+
+// let a = 7;
+// let b = 8;
+
+// let a = 7,
+//     b = 8,
+//     c = 214124;
+
+// const x = 'qewt', y = 555;
+
+// console.log(x);
+// console.log(y);
+
+// let x = 0;
+// console.log(x);
+
+// const y = 'Hello';
+// console.log(y);
+
+
+
+// Global and local vars
+
+// global
+// x = 123;
+// console.log(window.x);
+
+// local
+// const x = 111;
+// console.log(window.x);
+// let y = 123;
+
+
+// let x = y = 555, z, we = 'tqewwqt';
+
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+// Aryphm
+// console.log(Math.PI);
+
+// let cube = 3 * 3 * 3 * 3; 
+// cube = Math.pow(3, 4);
+
+// console.log(cube);
+
+// const a = 5, b = 9, c = 3;
+// const minimum = Math.min(a, b, c);
+
+// console.log(minimum);
+// console.log('minimum > ', minimum, 123213, minimum, ' 123 1231 312 31');
+
+
+// let a = 5;
+
+// console.log('a >', a);
+
+
+
+
+
+
+// Типи даних
+
+// 1. string
+// const str = 'some string';
+// console.log('str > ', str);
+// console.log(typeof str);
+
+// 2. number
+// const num = 123;
+// console.log('num > ', num);
+// console.log(typeof num);
+
+
+// 3. boolean
+// const ok = true;
+// const no = false;
+
+// console.log('ok > ', ok);
+// console.log(typeof ok);
+
+// console.log('no > ', no);
+// console.log(typeof no);
+
+// 4. undefined
+// let x;
+// let X = undefined;
+// console.log('x > ', x);
+// console.log(typeof x);
+
+// 5. null
+
+// let y = null;
+// console.log('y > ', y);
+// console.log(typeof y);
+
+
+
+// Перетворення типів
+
+// let qwe = 141241;
+// console.log('qwe > ', qwe);
+// console.log(typeof qwe);
+
+// let qwee = true;
+// console.log('qwee > ', qwee);
+// console.log(typeof qwee);
+
+
+
+
+
+
+
+const str1 = 'First string';
+const number1 = 1111111;
+const bool1 = false;
+
+
+// ...
+// ...
+// ...
+// ...
+// ...
+// ...
+
+// let output = number1 + str1 + bool1;
+
+// console.log('output >', output);
+// console.log(typeof output);
+
+
+const str2 = '2432';
+// console.log('str2 >', str2);
+// console.log(typeof str2);
+// console.log(typeof str1);
+
+
+// console.log(str2 + str1);
+
+
+
+// const num3 = 333;
+// const str3 = '4242';
+
+// console.log(typeof num3);
+// console.log(typeof +str3);
+
+// console.log(num3 + +str3);
+
+
+// const str4 = 'stroka4';
+// const num4 = 235235235;
+
+// console.log('str4 + num4 =', str4 + num4);
+
+
+// const num7 = '444.634';
+
+// const result = parseInt(num7);
+// const result = parseFloat(num7);
+
+// console.log(result);
+
+
+
+// const notAnumber = prompt('What?');
+// console.log(+notAnumber);
+
+// let A = NaN;
+// console.log('A > ', A);
+// console.log(typeof A);
+
+
+
+// const someNum = prompt('Enter number:');
+// const result = +someNum / -0;
+
+// console.log(result);
+// console.log(typeof result);
+
+// const numb5 = 124124124;
+// console.log(numb5.toString());
