04636dd8abf4d2ac4f5fa494620bc6dc3118eae7
roman-yaremiy <ryaremiy@conceptsoft.io>
2023-09-28 18:50:29 +0300

    lesson 4 added

---
+++ 4/script.js
@@ -0,0 +1,18 @@
+// switch (a) {
+//     case 1: // if (x === 1)
+//         alert('');
+//         break;
+//     case 2:
+//         alert('');
+//         break;
+//     case 3:
+//         alert('');
+//         break;
+//     defaul1:
+//         alert('');
+// }
+
+// alert( false || false ); // false
+// alert( false || true );  // true
+// alert( true || false );  // true
+// alert( true || true );   // true
