27cfa4fe746ccd3efde6099d38554aa0d2d83657
roman-yaremiy <ryaremiy@conceptsoft.io>
2024-02-01 21:27:16 +0200

    5 done

--- 5/script.js
+++ 5/script.js
@@ -1,285 +1,308 @@
-// const year = +prompt('Enter year: ', 0);
+// Визначити, чи є рік високосним, чи ні.
+// Рік буде високосним, якщо він кратний (тобто ділиться без залишку)
+//  на 400 або він одночасно кратний 4 і не кратний 100.
 
-// year % 0 === 0 => true
-// year % 0 => false  =>  0
-// !(year % 0) => !false => true
+// const year = +prompt('What is the year?');
 
-// if (true) alert('1');
-
-// if (!(year % 400) || (!(year % 4) && year !== 100)) {
-//     alert('Високосний');
-// } else {
-//     alert('-Високосний');
+// if (!(year % 400) || (!(year % 4) && (year / 100))) {
+// if (year % 400 === 0) {
+// alert('Високосний рік');
 // }
 
+// console.log(1600 % 400 === 0);
+// console.log(Boolean(1600 % 400));
 
-// const variable = (!(year % 400) || (!(year % 4) && year !== 100)) ? alert('Високосний') : alert('-Високосний');
+// const a = 5;
+// const b = 6;
+// const x = 100;
 
-
-// const variable = 1;
-
-// const arr2 = [];
-// const arr3 = new Array(5);
-
-// const arrMix = [1, 3, '234', 'string', null, undefined, NaN, {}];
-
-// console.log(arrMix);
-
-
-// console.log('typeof > ', typeof arr);
-
-// const arr = [1, 3, 6, 18, 21];
-//          // [0, 1, 2, 3, 4];
-
-// console.log(arr[3]);
-
-// arr[3] = 333;
-
-// console.log(arr[3]);
-
-
-
-// let a = 15;
-// console.log('a > ', a);
-
-// a = 25;
-// console.log('a > ', a);
-
-// const ab = [];
-
-
-// arr[1] = 44;
-// arr[2] = 66;
-
-// const arr = [2, 4, 6, 7, 8, 9, 62];
-// const arrLength = arr.length;
-
-// console.log('arrLength > ', arrLength);
-
-// const lastElement = arr[arr.length - 1];  // es4
-// const lastElement3 = arr.at(-2);  // es6
-
-// console.log('lastElement > ', lastElement);
-
-
-
-
-// const arr = [5, 7, 7, 3, 7, 12];
-
-// // arr[2] = 77;
-
-// // console.log('arr > ', arr);
-
-// arr.shift();
-
-// // console.log('arr > ', arr);
-
-// arr.unshift(111);
+// const arr = [1, 4, 6];
 // console.log('arr > ', arr);
 
-// arr.unshift(112);
-// console.log('arr > ', arr);
+// const emptyArr = [];
+// console.log('emptyArr > ', emptyArr);
 
-// arr.unshift(113);
-// console.log('arr > ', arr);
+// const numberArr = [1, 4, 6];
+// console.log('numberArr > ', numberArr);
 
+// const atringArr = ['first', 'second', 'third'];
+// console.log('atringArr > ', atringArr);
 
-// arr.push(14);
-// arr.push(18, 21);
-// console.log('arr > ', arr);
+// const nullArr = [null, null];
+// console.log('nullArr > ', nullArr);
 
-// arr.pop();
-// console.log('arr > ', arr);
+// const boolArr = [true, false, false];
+// console.log('boolArr > ', boolArr);
 
+// const randomArr = [234532, 'false', false, null, undefined, '3245'];
+// console.log('randomArr > ', randomArr);
 
+// const emptyArr = ['elem1', 'elem2', 'elem3'];
+// const secondArr = new Array('elem1', 'elem2', 'elem3');
 
-// ЧЕРГА
-// const queue = [5, 7, 3, 7, 12];
-// console.log('queue > ', queue);
+//              0   1   2   3   4
+// const newArr = [25, 62, 72, 36, 5];
+// console.log(newArr);
 
-// queue.push(333);
-// console.log('queue > ', queue);
+// // newArr.length
+// const arrayLength = newArr.length;
+// console.log(arrayLength);
 
+//              0   1   2   3   4   5
+// const newArr = [25, 62, 72, 36, 5, 88];
+
+// // newArr[0]
+// // newArr[2]
+
+// console.log(newArr);
+
+// newArr[2] = 222;
+// newArr[4] = 567;
+
+// console.log(newArr[4]);
+// console.log(newArr[2]);
+
+// console.log(newArr.length);
+
+// // newArr.length - 1   =  6 - 1
+// const lastElement = newArr[newArr.length - 1];
+// console.log(lastElement);
+
+// const beforeLastElement = newArr[newArr.length - 3];
+// console.log('beforeLastElement > ', beforeLastElement);
+
+// newArr.length = 0;
+// // console.log(newArr);
+
+// newArr.length = 130;
+// console.log(newArr);
+
+// const newArr = [25, 62, 72, , , , 36, 5, 88];
+// console.log('newArr >', newArr[3]);
+// console.log('length >', newArr.length);
+
+// console.log('newArr >', newArr[333]);
+// console.log('newArr >', newArr[-25]);
+
+// const newArr = [25, 62, 72, 36, 5, 88];
+// console.log('newArr >', newArr);
+
+// newArr[3] = 555;
+// console.log('newArr >', newArr);
+
+// newArr[0] = 0;
+// console.log('newArr >', newArr);
+
+// const i = 2;
+// newArr[i]
+// newArr[(i + 2) / 1]
+// console.log('newArr >', newArr[(i + 2) / 1]);
+
+// newArr[0]
+// newArr[newArr.length - 1]
+
+// const newArr = [25, 62, 72, 36, 5, 88];
+// console.log('newArr >', newArr);
+
+// newArr.push(777);
+// console.log('newArr >', newArr);
+
+// newArr.push(888, 333, 432);
+// console.log('newArr >', newArr);
+
+// newArr.pop();
+// console.log('newArr >', newArr);
+
+// newArr.pop();
+// console.log('newArr >', newArr);
+
+// newArr.unshift(111);
+// console.log('newArr >', newArr);
+
+// newArr.unshift(112);
+// console.log('newArr >', newArr);
+
+// newArr.unshift(113, 115, 117);
+// console.log('newArr >', newArr);
+
+// newArr.shift();
+// console.log('newArr >', newArr);
+
+// newArr.shift();
+// console.log('newArr >', newArr);
+
+// queue
+// const queue = [25, 62, 72, 36, 5, 88];
+// const queue = [62, 72, 36, 5, 88 ];
+// console.log(queue);
+
+// const shiftedElement = queue.shift();
+// console.log(queue);
+// console.log('shiftedElement >', shiftedElement);
 
 // queue.shift();
-// console.log('queue > ', queue);
+// console.log(queue);
 
+// queue.push(99);
+// console.log(queue);
 
-// СТЕК
-// const stack = [1, 2, 3, 4, 5];
-// console.log('stack > ', stack);
+// queue.push(101);
+// console.log(queue);
 
-// stack.push('5');
-// stack.push('5');
-// console.log('stack > ', stack);
+// queue.shift();
+// console.log(queue);
+
+// stack
+// const stack = [25, 62, 72, 36, 5, 88];
+// console.log(stack);
+
+// const poppedElement = stack.pop();
+// console.log(stack);
+// console.log('poppedElement >', poppedElement);
 
 // stack.pop();
-// console.log('stack > ', stack);
+// console.log(stack);
 
+// stack.push(9);
+// console.log(stack);
 
+// stack.push(10);
+// console.log(stack);
 
+// stack.push(100);
+// console.log(stack);
 
-// let a = 5;
-// +a;
-// -a;
+// const index = (1 + 1 + 1);
+// console.log('index > ', index);
 
+// const stack = [25, 62, 72, 36, 5, 88];
 
-// a++;
+// const firstElem = stack[index - 1];
 
-// console.log('a > ', a);
+// console.log("firstElem > ", firstElem);
 
-// a--;
-// a--;
-// console.log('a > ', a);
+// # while
+// # do while
+// # for
 
-
-// console.log('a > ', ++a);
-// console.log('a > ', a);
-
-
-
-
-// let x = 1;
-
-// while (x <= 5) {
-//     console.log(x);
-
-//     x++;
+// while (condition) {
+//     // operation1;
+//     // operation2;
 // }
 
-
-
-
-
-// let x = 1;
+// let sum = 10;
+// let steps = 5;
+// console.log('start sum > ', sum);
 
 // while (true) {
-//     console.log(x);
+// while (steps < 0) {
+// while (steps === 0) {
+// while (steps > 0) {
+//     console.log('-----------------------------------');
+//     sum += 3;
+//     console.log('sum > ', sum);
 
-//     x++;
+//     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//     steps++;
+//     console.log('steps > ', steps);
 // }
 
+// console.log('-----------------------------------');
+// console.log('end sum > ', sum);
 
+// let sum = 10;
+// let steps = 5;
+// console.log('start sum > ', sum);
 
-
-// let x = 8;
-
-
-// while (x <= 5) {
-//     console.log(x);
-
-//     x++;
-// }
-
+// let doLoop = true;
 
 // do {
-//     console.log(x);
+//     console.log('-----------------------------------');
+//     sum += 3;
+//     console.log('sum > ', sum);
 
-//     x++;
-// } while (x <= 5);
+//     steps--;
 
+//     if (steps <= 0) {
+//         doLoop = false;
+//     }
+//     console.log('steps > ', steps);
+// } while (doLoop);
 
+// console.log('-----------------------------------');
+// console.log('end sum > ', sum);
 
-// let x = 5;
+// let sum = 10;
+// let steps = 5;
+// console.log("start sum > ", sum);
 
-// for (let i = 1; i <= 5; i++) {
-//     console.log(i);
+// for (let i = 0; i < steps; i++) {
+//     console.log(`------ Step #${i + 1} ----------------`);
+//     sum += 3;
 
+//     console.log("sum > ", sum);
+//     console.log("i > ", i);
 // }
 
+// console.log("end sum > ", sum);
 
-// let i = 1;
+// const newArr = [25, 62, 72, 36, 5, 88];
+// console.log(newArr);
 
-// for (; ;) {
-//     console.log(i);
+// for (let i = 0; i < newArr.length; i++) {
+//     console.log(`------ Step #${i + 1} ----------------`);
+//     console.log("i > ", i);
+//     console.log(`newArr[${i}] > `, newArr[i]);
+// }
 
-//     if (i >= 5) {
-//         break;
+// # Виведіть парні числа
+
+// const newArr = [25, 62, 72, 36, 5, 88];
+
+// // const k = 1;
+
+// for (let i = 0; i < newArr.length; i++) {
+//     if (newArr[i] % 2 === 0) {
+//         console.log(newArr[i]);
+//     }
+// }
+
+// # Виведіть максимальне число з масива
+
+
+
+
+
+// const newArr = [25, 62, 88, 72, 36, 5];
+// console.log("newArr > ", newArr);
+
+
+// let maxValue = newArr[0];
+
+
+
+// for (let i = 0; i < newArr.length; i++) {
+//     console.log(`newArr[${i}] > ${maxValue}`);
+
+//     if (newArr[i] > maxValue) {
+//         maxValue = newArr[i];
 //     }
 
-//     i++;
 // }
 
 
+// console.log('maxValue > ', maxValue);
 
 
+// # Інверсія масиву
 
+// const newArr = [25, 62, 88, 72, 36, 5];
+// const invertedArr = [];
 
-
-// Вивести парні числа
-// const numbers = [2, 5, 6, 8, 3, 5, 6, 1, 2, 3, 5, 6, 78, 5, 4, 64, 5624, 5624, 7534, 73];
-
-// for (let i = 0; i < numbers.length - 1; i++) {
-//     if (numbers[i] % 2 === 0) {
-//         console.log(numbers[i]);
-//     }
+// for (let i = newArr.length - 1; i >= 0; i--) {
+//         console.log(newArr[i]);
+//         invertedArr.push(newArr[i]);
 // }
 
-
-
-
-// Вивести максимальне число з масива
-// const numbers = [2, 5, 6, 8, 3, 5, 6, 7534, 1, 2, 3, 5, 6, 78, 5, 4, 64, 5624, 73];
-// let result = null;
-
-// for (let i = 0; i < numbers.length - 1; i++) {
-//     if (i === 0) {
-//         result = numbers[0];
-//         continue;
-//     }
-
-
-//     if (numbers[i] > numbers[i - 1] && numbers[i] > result) {
-//         result = numbers[i];
-//     } else if (numbers[i - 1] > result) {
-//         result = numbers[i - 1];
-//     }
-
-
-// }
-
-// console.log('result > ', result);
-
-
-
-
-
-
-// Інверсія
-// const numbers = [2, 5, 5, 6, 7534, 1, 2, 78, 5, 4, 64, 5624, 73];
-// const reversed = [];
-
-// console.log(numbers);
-
-// for (let i = 0; i <= numbers.length - 1; i++) {
-
-//     reversed[i] = numbers[numbers.length - 1 - i];
-
-
-// }
-
-// console.log(reversed);
-
-
-
-const doIwish = confirm('Do you wish to resgister?');
-
-if (doIwish) {
-    // YES
-    const currentYear = new Date().getFullYear();
-    let yearOfBirth = +prompt('Ваш рік народженя', 1920);
-
-    while (currentYear - yearOfBirth < 18) {
-        yearOfBirth = +prompt('Ваш рік народженя', 1920);
-    }
-
-    alert("Welcome on board!!! Continue registration!");
-    
-    // else {
-    //     alert("Not today");
-    // }
-
-} else {
-    // NO
-    alert('До побачення!!!');
-}
+// console.log(' newArr > ', newArr);
+// console.log(' invertedArr > ', invertedArr);
