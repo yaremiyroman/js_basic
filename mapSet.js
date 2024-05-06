bb8118f1235653643006c3ed4e9c668c0fc791bb
andrdrx <yaremiyroman@gmail.com>
2024-05-06 19:15:32 +0300

    10

--- mapSet.js
+++ mapSet.js
@@ -125,6 +125,27 @@ map.forEach((value, key, map) => {
     // Код для виконання з value, key та map
 });
 
+// Reverse Map:
+// Write a function that takes a Map and returns a new Map where the keys and values are swapped.
+function swapKeysAndValues(map) {
+    const swappedMap = new Map();
+    map.forEach((value, key) => {
+        swappedMap.set(value, key);
+    });
+    return swappedMap;
+}
+
+// Example usage:
+const originalMap = new Map([
+    ['a', 1],
+    ['b', 2],
+    ['c', 3]
+]);
+
+const swappedMap = swapKeysAndValues(originalMap);
+console.log(swappedMap); // Output: Map(3) { 1 => 'a', 2 => 'b', 3 => 'c' }
+
+
 
 
 // Delete
@@ -194,7 +215,14 @@ console.log(name, age);
 // WeakMap ?////////////////////////////////////////////?////////////////////////////////////////////?////////////////////////////////////////////
 
 
+// let key = {
+//     name: 'John'
+// };
 
+// const weakMap = new WeakMap([[key, 1]]);
+
+// key = null;
+// console.log(weakMap.get(key)); // undefined
 
 
 
@@ -267,6 +295,16 @@ let mySet2 = new Set([1, 2, 3, 4]);
 mySet2.forEach(function (value) {
     console.log(value);
 });
+
+
+// Remove Duplicates:
+// Write a function that takes an array as input and returns a Set containing only the unique elements of the array.
+const numbers = [1, 2, 3, 3, 4, 5, 5];
+const uniqueNumbers = [...new Set(numbers)];
+console.log(uniqueNumbers); // Виведе: [1, 2, 3, 4, 5]
+
+
+
 
 
 // delete
@@ -352,16 +390,16 @@ console.log(differenceSet); // Set { 1, 2 }
 
 
 
-// SymmetricDifference
-const setA11 = new Set([1, 2, 3]);
-const setB11 = new Set([3, 4, 5]);
+// // SymmetricDifference
+// const setA11 = new Set([1, 2, 3]);
+// const setB11 = new Set([3, 4, 5]);
 
-// Створюємо нову множину, що містить елементи, присутні тільки в одній із множин
-const symmetricDifferenceSet = new Set(
-    [...setA11].filter(x => !setB11.has(x)).concat([...setB11].filter(x => !setA11.has(x)))
-);
+// // Створюємо нову множину, що містить елементи, присутні тільки в одній із множин
+// const symmetricDifferenceSet = new Set(
+//     [...setA11].filter(x => !setB11.has(x)).concat([...setB11].filter(x => !setA11.has(x)))
+// );
 
-console.log(symmetricDifferenceSet); // Set { 1, 2, 4, 5 }
+// console.log(symmetricDifferenceSet); // Set { 1, 2, 4, 5 }
 
 
 
@@ -430,39 +468,6 @@ for (const value of set) {
 
 
 
-// Reverse Map:
-// Write a function that takes a Map and returns a new Map where the keys and values are swapped.
-function swapKeysAndValues(map) {
-    const swappedMap = new Map();
-    map.forEach((value, key) => {
-        swappedMap.set(value, key);
-    });
-    return swappedMap;
-}
-
-// Example usage:
-const originalMap = new Map([
-    ['a', 1],
-    ['b', 2],
-    ['c', 3]
-]);
-
-const swappedMap = swapKeysAndValues(originalMap);
-console.log(swappedMap); // Output: Map(3) { 1 => 'a', 2 => 'b', 3 => 'c' }
-
-
-
-
-
-
-
-
-
-// Remove Duplicates:
-// Write a function that takes an array as input and returns a Set containing only the unique elements of the array.
-const numbers = [1, 2, 3, 3, 4, 5, 5];
-const uniqueNumbers = [...new Set(numbers)];
-console.log(uniqueNumbers); // Виведе: [1, 2, 3, 4, 5]
 
 
 
@@ -473,179 +478,51 @@ console.log(uniqueNumbers); // Виведе: [1, 2, 3, 4, 5]
 
 
 
-
-// Вам необхідно написати функцію, яка приймає на вхід масив об'єктів, де кожен об'єкт описує сповіщення та має поля 'source' / text / date. Вам необхідно перетворити цей масив на об'єкт, де ключем буде джерело сповіщення, а значенням - масив сповіщень із цього джерела.
-
-const data = [
-    {
-        'source': 'ukr.net',
-        'text': 'Вам необхідно написати функцію 1 1 1 1',
-        'date': '23.11.1989',
-    },
-    {
-        'source': 'google.com',
-        'text': 'Кожен об\'єкт описує сповіщення та має поля 1 1 1 1',
-        'date': '01.01.2022',
-    },
-    {
-        'source': 'unian.ua',
-        'text': 'Вам необхідно перетворити цей масив на об\'єкт 1 1 1 1',
-        'date': '05.12.2024',
-    },
-    {
-        'source': 'ukr.net',
-        'text': 'Вам необхідно написати функцію 1 2 2 2 2 ',
-        'date': '23.11.1889',
-    },
-    {
-        'source': 'google.com',
-        'text': 'Кожен об\'єкт описує сповіщення та має поля 2 2 2 2 ',
-        'date': '01.12.2002',
-    },
-    {
-        'source': 'unian.ua',
-        'text': 'Вам необхідно перетворити цей масив на об\'єкт 2 2 2 2 ',
-        'date': '05.12.2024',
-    },
-    {
-        'source': 'ukr.net',
-        'text': 'Вам необхідно написати функцію 21555',
-        'date': '23.11.1989',
-    },
-    {
-        'source': 'google.com',
-        'text': 'Кожен об\'єкт описує сповіщення та має поля1555',
-        'date': '01.42.2002',
-    },
-    {
-        'source': 'unian.ua',
-        'text': 'Вам необхідно перетворити цей масив на об\'єкт1555',
-        'date': '05.12.2024',
-    },
-    {
-        'source': 'ukr.net',
-        'text': 'Вам необхідно написати функцію 332',
-        'date': '23.11.1989',
-    },
-    {
-        'source': 'google.com',
-        'text': 'Кожен об\'єкт описує сповіщення та має поля32',
-        'date': '01.01.2002',
-    },
-    {
-        'source': 'unian.ua',
-        'text': 'Вам необхідно перетворити цей масив на об\'єкт32',
-        'date': '05.12.2024',
-    },
-    {
-        'source': 'ukr.net',
-        'text': 'Вам необхідно написати функцію 43',
-        'date': '23.11.1989',
-    },
-    {
-        'source': 'google.com',
-        'text': 'Кожен об\'єкт описує сповіщення та має поля3',
-        'date': '01.01.2002',
-    },
-    {
-        'source': 'unian.ua',
-        'text': 'Вам необхідно перетворити цей масив на об\'єкт3',
-        'date': '05.12.2024',
-    },
-];
-
-// 1st
-// const msgsCollection = new Map();
-
-// const mapMessagesToSource = msgs =>
-//     Object.groupBy(msgs, (msg, i) => {
-//         msgsCollection.set(msg.source + i, msg);
-//         return msg.source;
-//     });
-
-// console.log(mapMessagesToSource(data));
-
-// for (let message of msgsCollection.values()) {
-//     console.log(message);
-// }
-
-
-// With iterator
-
-const mapMessagesToSource = msgs => {
-    const groupedMessages = Object.groupBy(msgs, (msg, i) => {
-        return msg.source;
-    });
-
-    return groupedMessages[Symbol.iterator] = function () {
+const myIterable = {
+    items: [1, 2, 3, 4],
+    [Symbol.iterator]() {
         let index = 0;
-        const keys = Object.keys(this);
 
         return {
             next: () => {
-                if (index < keys.length) {
-                    return { value: keys[index++], done: false };
+                if (index < this.items.length) {
+                    return { value: this.items[index++], done: false };
                 } else {
                     return { done: true };
                 }
             },
         };
-    };
-}
+    },
+};
 
-// Call
-const groups = mapMessagesToSource(msgs);
-
-for (let group of groups) {
-    console.log(groups[group]);
+for (const item of myIterable) {
+    console.log(item);
 }
 
 
 
 
-// Memo
-// Встановіть обмеження на розмір кеша у вигляді числа N. Якщо це значення перевищено, то вам необхідно перезаписати перше значення, потім друге і так далі.
-// Додайте перевірку, щоб прибрати дублікати результатів із кешу
-const multiple = (a, b) => +a * +b;
 
-function memo(fn, N) {
-    let cache = new Map();
-    let counter = 0;
 
-    return function (...args) {
-        const newKey = args.join('-');
 
-        if (cache.has(newKey)) {
-            console.log("CACHED", cache);
-            return cache.get(newKey);
-        }
 
-        if (cache.size === N) {
-            console.log("cache.size === N", cache.size, ' = ', N);
-            const entries = [...cache.entries()];
-            const keys = [...cache.keys()];
-            const cacheItemIndex = entries.findIndex(entry => entry.includes(keys[counter]));
 
-            if (++counter === N) {
-                counter = 0;
-            }
-
-            if (!!entries[cacheItemIndex]) {
-                entries[cacheItemIndex][0] = newKey;
-                cache = new Map(entries);
-                cache.set(newKey, fn(...args));
-                console.log("cacheItemIndex");
-                return cache.get(newKey);
-            }
-        }
-
-        console.log("SET");
-        cache.set(newKey, fn(...args));
-
-        return cache.get(newKey);
-    }
+const naturalRowIterator = {
+    [Symbol.iterator]: () => ({
+        _current: 0,
+        next() { return {
+            value: ++this._current,
+            done: this._current > 3,
+       }},
+   }),
 }
 
-const memoMultiplication = memo(multiple, 3);
+for (num of naturalRowIterator) {
+    console.log(num);
+}
+// Вывод: 1, 2, 3
 
 
+
+
+// Ітерація по всім властивостям обʼєкта
