ae2657ef687a81fa8bcab4360064188071afb498
andrdrx <yaremiyroman@gmail.com>
2024-06-02 12:06:49 +0300

    Program updated

---
+++ exercises/dataTypesConversion_1.js
@@ -0,0 +1,15 @@
+undefined + 1;
+'true' == true
+false == 'false'
+null == ''
+3 ** (9 / 3);
+!!'false' == !!'true'
+0 || '0' && 1
+1 < 2 < 3
+'foo'+ + +'bar'
+3 ** 2 / 3;
+1 < 2 > 3
+(+null == false) < 1;
+false && true || true
+false && (true || true);
+(+null == false) < 1 ** 5;
